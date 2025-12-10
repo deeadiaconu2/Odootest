95a09109336b3cf237eeb6b159716fb414cee5fa
deeadiaconu2 <adiaconu617@gmail.com>
2025-12-10 20:51:20 +0100

    v1

---
+++ extra-addons/biblioteca_alumno/models/__init__.py
@@ -0,0 +1,2 @@
+# extra-addons/biblioteca_alumno/models/__init__.py
+from . import biblioteca_libro
---
+++ extra-addons/biblioteca_alumno/models/biblioteca_libro.py
@@ -0,0 +1,12 @@
+# extra-addons/biblioteca_alumno/models/biblioteca_libro.py
+from odoo import models, fields
+
+class BibliotecaLibro(models.Model):
+    _name = "biblioteca.libro"
+    _description = "Libro de la biblioteca (práctica DAM)"
+
+    name = fields.Char("Título", required=True)
+    autor = fields.Char("Autor")
+    fecha_publicacion = fields.Date("Fecha de publicación")
+    paginas = fields.Integer("Número de páginas")
+    disponible = fields.Boolean("Disponible", default=True)
