197cc6ad90d1ab43bec5b818834202a3e2e725bb
deeadiaconu2 <adiaconu617@gmail.com>
2025-12-12 19:42:54 +0100

    corregir biblioteca

--- extra-addons/biblioteca_alumno/models/biblioteca_libro.py
+++ extra-addons/biblioteca_alumno/models/biblioteca_libro.py
@@ -1,10 +1,10 @@
 # extra-addons/biblioteca_alumno/models/biblioteca_libro.py
 from odoo import models, fields
 class BibliotecaLibro(models.Model):
-_name = "biblioteca.libro"
-_description = "Libro de la biblioteca (práctica DAM)"
-name = fields.Char("Título", required=True)
-autor = fields.Char("Autor")
-fecha_publicacion = fields.Date("Fecha de publicación")
-paginas = fields.Integer("Número de páginas")
-disponible = fields.Boolean("Disponible", default=True)
+	_name = "biblioteca.libro"
+	_description = "Libro de la biblioteca (práctica DAM)"
+	name = fields.Char("Título", required=True)
+	autor = fields.Char("Autor")
+	fecha_publicacion = fields.Date("Fecha de publicación")
+	paginas = fields.Integer("Número de páginas")
+	disponible = fields.Boolean("Disponible", default=True)
